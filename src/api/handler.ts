9663d2e0a139ce02a37526ed3e898d9403212dcb
Abhishek Chadha <abhishekchadha235@gmail.com>
2017-12-10 18:25:42 -0800

    add server basics, readme, changelog

---
+++ src/api/handler.ts
@@ -0,0 +1,10 @@
+export function hello(event, context, callback) {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+  callback(null, response);
+};
